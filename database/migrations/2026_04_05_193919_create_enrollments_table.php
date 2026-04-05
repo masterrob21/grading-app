c3356eea45013082ee69b93391b94df3b5ea791a
Robert Quainoo <softdeveloperrob@gmail.com>
2026-04-05 21:52:50 +0000

    created model, migration for enrollments

---
+++ database/migrations/2026_04_05_193919_create_enrollments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('enrollments', function (Blueprint $table) {
+            $table->id();
+            $table->string('student_id');
+            $table->foreignId('course_id');
+            $table->foreignId('academic_year_id');
+            $table->timestamps();
+
+            $table->foreign('student_id')->references('student_id')->on('students')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('enrollments');
+    }
+};
