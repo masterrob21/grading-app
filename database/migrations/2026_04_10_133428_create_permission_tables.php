ff283ba0885accee2efa15de8662982f4de50b70
Robert Quainoo <softdeveloperrob@gmail.com>
2026-04-10 15:53:21 +0000

    modified the migrations fields

--- database/migrations/2026_04_10_133428_create_permission_tables.php
+++ database/migrations/2026_04_10_133428_create_permission_tables.php
@@ -27,6 +27,7 @@ return new class extends Migration
             $table->id(); // permission id
             $table->string('name');
             $table->string('guard_name');
+            $table->string('module');
             $table->timestamps();
 
             $table->unique(['name', 'guard_name']);
